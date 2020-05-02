02283204c6d7c8fb11e6c5c36d976fd4d250fae5
AntoineG <antoine.garcia@polyconseil.fr>
2020-05-02 21:35:19 +0200

    do not greet on arrival

--- equities/views.py
+++ equities/views.py
@@ -10,10 +10,6 @@ def index(request):
 
 
 def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
