80d20ec5ef51100f9cdc7c5bf1884999853ead75
AntoineG <antoine.garcia@polyconseil.fr>
2020-05-05 13:31:37 +0200

    add function to download from admin

--- equities/admin.py
+++ equities/admin.py
@@ -1,10 +1,31 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from equities import models
 
 
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+
 @admin.register(models.Gap)
-class GapAdmin(admin.ModelAdmin):
+class GapAdmin(admin.ModelAdmin, ExportCsvMixin):
     date_hierarchy = "timestamp"
     list_display = [
         "symbol",
@@ -25,3 +46,4 @@ class GapAdmin(admin.ModelAdmin):
         "timestamp",
     ]
     search_fields = ("symbol",)
+    actions = ["export_as_csv"]
